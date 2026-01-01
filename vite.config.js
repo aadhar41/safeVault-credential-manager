e34653b5591c32bc12e2631c686e5cc1e869c8c1
Aadhar gaur <aadhar41@gmail.com>
2026-01-01 21:23:34 +0530

    feat: Set up initial project structure, configure app name, add basic styling, and include lucide-react dependency.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
+    base: './',
     root: '.',               // default – project root
     publicDir: 'public',    // optional, if you have static assets
 });
